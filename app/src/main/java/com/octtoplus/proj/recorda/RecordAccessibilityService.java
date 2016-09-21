1823a91f50061e20d7a369e37c8abc5ca38fc48c
Muangsiri Woramet <krw127@hotmail.com>
2016-09-21 14:25:43 +0900

    init

---
+++ app/src/main/java/com/octtoplus/proj/recorda/RecordAccessibilityService.java
@@ -0,0 +1,51 @@
+package com.octtoplus.proj.recorda;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by muangsiriworamet on 9/21/16.
+ */
+
+public class RecordAccessibilityService extends AccessibilityService {
+
+    private final AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+    private static final String TAG = "RecService";
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        final int eventType = accessibilityEvent.getEventType();
+        String eventText = null;
+        
+        switch(eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "Clicked: ";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED:
+                eventText = "Focused: ";
+                break;
+        }
+        
+        eventText = eventText + accessibilityEvent.getContentDescription();
+        Log.i(TAG, "Event: " + eventText);
+        
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        Log.i(TAG, "onServiceConnected: asdfadfasdfasdfasdfasdfdsafdsafs");
+        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED | AccessibilityEvent.TYPE_VIEW_FOCUSED;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_HAPTIC;
+        info.packageNames = new String[]{"com.octtoplus.proj.recorda"};
+        info.notificationTimeout = 100;
+
+        this.setServiceInfo(info);
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
