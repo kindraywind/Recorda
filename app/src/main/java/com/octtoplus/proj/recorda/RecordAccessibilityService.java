00a1f73a99452aa53f3aae97b4053c057c2b9677
Muangsiri Woramet <krw127@hotmail.com>
2016-09-27 16:23:23 +0900

    Add toast for debugging purpose

--- app/src/main/java/com/octtoplus/proj/recorda/RecordAccessibilityService.java
+++ app/src/main/java/com/octtoplus/proj/recorda/RecordAccessibilityService.java
@@ -2,8 +2,10 @@ package com.octtoplus.proj.recorda;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
 
 /**
  * Created by muangsiriworamet on 9/21/16.
@@ -18,7 +20,6 @@ public class RecordAccessibilityService extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
         final int eventType = accessibilityEvent.getEventType();
         String eventText = null;
-
         
         switch(eventType) {
             case AccessibilityEvent.TYPE_VIEW_CLICKED:
@@ -27,23 +28,26 @@ public class RecordAccessibilityService extends AccessibilityService {
             case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED:
                 eventText = "Focused: ";
                 break;
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                eventText = "Scrolled: ";
+                break;
         }
         
         eventText = eventText + accessibilityEvent.getContentDescription();
         Log.i(TAG, "Event: " + eventText);
         Log.i(TAG, "a: "+accessibilityEvent.toString());
+        toast("Event:"+ eventText);
+
         
     }
 
     @Override
     protected void onServiceConnected() {
         Log.i(TAG, "onServiceConnected: asdfadfasdfasdfasdfasdfdsafdsafs");
-        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
-                AccessibilityEvent.TYPE_VIEW_SCROLLED |
-                AccessibilityEvent.TYPE_VIEW_LONG_CLICKED |
-                AccessibilityEvent.TYPE_VIEW_FOCUSED |
-                AccessibilityEvent.TYPE_VIEW_SELECTED;
-        //info.feedbackType = AccessibilityServiceInfo.FEEDBACK_HAPTIC;
+        toast("Start recorda");
+
+        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED | AccessibilityEvent.TYPE_VIEW_SCROLLED;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_HAPTIC;
         info.packageNames = new String[]{"com.octtoplus.proj.recorda"};
         info.notificationTimeout = 100;
 
@@ -53,5 +57,14 @@ public class RecordAccessibilityService extends AccessibilityService {
     @Override
     public void onInterrupt() {
 
+    }
+
+    public void toast(CharSequence text) {
+        Context context = getApplicationContext();
+
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
