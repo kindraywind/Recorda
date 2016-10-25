924dc6ea83d709764708258a46377201fa026d48
Kittinun Vantasin <kittinun.f@gmail.com>
2016-10-25 23:22:28 +0900

    Improve Kotlin semantic to project

---
+++ app/src/main/kotlin/com/octtoplus/proj/recorda/JSONBuilder.kt
@@ -0,0 +1,73 @@
+package com.octtoplus.proj.recorda
+
+import org.json.JSONObject
+
+class JSON private constructor() {
+
+    companion object {
+        fun create(block: JSONBuilder.() -> Unit) = JSONBuilder(block).build()
+    }
+
+    class JSONBuilder private constructor() {
+
+        constructor(init: JSONBuilder.() -> Unit) : this() {
+            init()
+        }
+
+        var className: CharSequence? = null
+        var contentDescription: CharSequence? = null
+        var eventTime: Long? = null
+        var eventType: CharSequence? = null
+        var resourceIdName: CharSequence? = null
+
+        fun className(block: JSONBuilder.() -> CharSequence) = apply { className = block() }
+
+        fun contentDescription(block: JSONBuilder.() -> CharSequence) = apply { contentDescription = block() }
+
+        fun eventTime(block: JSONBuilder.() -> Long) = apply { eventTime = block() }
+
+        fun eventType(block: JSONBuilder.() -> CharSequence) = apply { eventType = block() }
+
+        fun resourceIdName(block: JSONBuilder.() -> CharSequence) = apply { resourceIdName = block() }
+
+        fun build() = JSONObject().build(this)
+
+        /*
+        put("className", event.className)
+        put("contentDescription", event.contentDescription)
+        put("eventText", getEventText(event))
+        put("eventTime", java.lang.Long.toString(event.eventTime))
+        put("eventType", AccessibilityEvent.eventTypeToString(event.eventType))
+        put("fromIndex", event.fromIndex)
+        put("isChecked", event.isChecked)
+        put("isEnable", event.isEnabled)
+        put("isPassword", event.isPassword)
+        put("itemCount", event.itemCount)
+        put("packageName", event.packageName)
+        put("toIndex", event.toIndex)
+        put("resource-id", event.source.viewIdResourceName)
+         */
+    }
+
+}
+
+fun JSONObject.build(builder: JSON.JSONBuilder): JSONObject {
+    apply {
+        builder.className?.let {
+            put("className", it.toString())
+        }
+        builder.contentDescription?.let {
+            put("contentDescription", it.toString())
+        }
+        builder.eventTime?.let {
+            put("eventTime", it.toString())
+        }
+        builder.eventType?.let {
+            put("eventType", it.toString())
+        }
+        builder.resourceIdName?.let {
+            put("resource-id", it.toString())
+        }
+    }
+    return this
+}
